6c15e556ae2f3d76c9ddc12537b9577f2771ee91
Tim Knoll <timothy.knoll@gmail.com>
2018-10-27 21:47:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,7 @@ class Artist
   attr_reader :songs
   extend Memorable::ClassMethods
   # include Memorable::InstanceMethods
+  extend Findable
 
   @@artists = []
 
@@ -14,9 +15,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
