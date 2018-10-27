a4d757fccda98e55a43ee01672fa637730b2d357
Tim Knoll <timothy.knoll@gmail.com>
2018-10-27 21:55:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,7 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
+  include Paramable
 
   @@songs = []
 
@@ -35,7 +36,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param # paramable module method
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
